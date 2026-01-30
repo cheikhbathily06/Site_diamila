aa09d7a7d34f357f68e6f63b93cf2a5d8034f582
amala <cheikhbath59@gmail.com>
2026-01-30 13:35:10 +0000

    Initial commit - Site Denisia BIjoux Pret pour production

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
+    secure: process.env.NODE_ENV === 'production', // HTTPS en production
+    httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7 // 7 jours
   }
 }));
